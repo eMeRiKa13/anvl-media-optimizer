e94b6b1d33f29bd96ba0ade4ffbb250ffe1574c8
Antoine Frankart <975898+eMeRiKa13@users.noreply.github.com>
2025-12-11 19:25:35 +0100

    Clean up files on launch

--- server/index.js
+++ server/index.js
@@ -19,8 +19,33 @@ app.use('/api', router);
 const uploadDir = path.join(__dirname, 'uploads');
 const processedDir = path.join(__dirname, 'processed');
 
-if (!fs.existsSync(uploadDir)) fs.mkdirSync(uploadDir);
-if (!fs.existsSync(processedDir)) fs.mkdirSync(processedDir);
+// Helper to empty a directory
+const emptyDirectory = (dir) => {
+    if (fs.existsSync(dir)) {
+        fs.readdirSync(dir).forEach(file => {
+            const curPath = path.join(dir, file);
+            if (fs.lstatSync(curPath).isDirectory()) { // recurse
+                 emptyDirectory(curPath);
+                 fs.rmdirSync(curPath);
+            } else { // delete file
+                 fs.unlinkSync(curPath);
+            }
+        });
+    }
+};
+
+// Ensure directories exist and are empty on startup
+if (fs.existsSync(uploadDir)) {
+    emptyDirectory(uploadDir);
+} else {
+    fs.mkdirSync(uploadDir);
+}
+
+if (fs.existsSync(processedDir)) {
+    emptyDirectory(processedDir);
+} else {
+    fs.mkdirSync(processedDir);
+}
 
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
